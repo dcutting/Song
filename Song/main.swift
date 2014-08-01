895377cd080306f8da675059fd4d0a2b9785cd46
Dan Cutting <dcutting@gmail.com>
2014-08-01 19:46:35 +0100

    Add unit testing target.

--- Song/main.swift
+++ Song/main.swift
@@ -1,12 +1,1 @@
-//
-//  main.swift
-//  Song
-//
-//  Created by Dan Cutting on 1/08/2014.
-//  Copyright (c) 2014 Yellowbek. All rights reserved.
-//
-
-import Foundation
-
-println("Hello, World!")
-
+println("Song")
