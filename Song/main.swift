2498c0d57e0ced0bd9f16dc325f07ef52c32c61c
Dan Cutting <dcutting@gmail.com>
2014-08-01 19:30:24 +0100

    Initial Commit

---
+++ Song/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Song
+//
+//  Created by Dan Cutting on 1/08/2014.
+//  Copyright (c) 2014 Yellowbek. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
