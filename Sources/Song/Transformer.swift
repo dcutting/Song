da4cbdc3aae0ee99cf0c06d1dcf949290a2b042c
Dan Cutting <dan@cutting.io>
2018-02-19 12:02:45 +0000

    Fix transform change accidentally committed.

--- Sources/Song/Transformer.swift
+++ Sources/Song/Transformer.swift
@@ -17,9 +17,8 @@ public func makeTransformer() -> Transformer<Expression> {
         .booleanValue(false)
     }
 
-    t.rule(["stringValue": .simple("s")]) { _ in
-    throw TransformerError.notNumeric("4")
-//    .stringValue(try $0.str("s"))
+    t.rule(["stringValue": .simple("s")]) {
+        .stringValue(try $0.str("s"))
     }
 
     t.rule(["integerValue": .simple("i")]) {
