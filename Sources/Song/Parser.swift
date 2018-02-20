a1bd1b13a054330c1e6e950bc38134c92ea8d9a5
Dan Cutting <dan@cutting.io>
2018-02-20 16:00:51 +0000

    Tidying up the parser and transformer.

--- Sources/Song/Parser.swift
+++ Sources/Song/Parser.swift
@@ -14,148 +14,124 @@ public func makeParser() -> ParserProtocol {
     let lParen = str("(") >>> skip
     let rParen = str(")")
     let quote = str("\"")
-    let escape = str("\\")
-    let digit = (0...9).match
+    let backslash = str("\\")
     let underscore = str("_")
+    let digit = (0...9).match
     let lowercaseLetter = "abcdefghijklmnopqrstuvwxyz".match
     let uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".match
     let letter = lowercaseLetter | uppercaseLetter
-    let symbol = dot | pipe | comma | lBracket | rBracket | lParen | rParen
-    let character = Deferred()
-    character.parser = letter | digit | space | symbol | escape >>> (escape | quote | character)
-    let times = str("*")
-    let dividedBy = str("/")
-    let modulo = str("%")
+    let symbol = dot | pipe | comma | lBracket | rBracket | lParen | rParen | underscore
+    let stringChar = Deferred()
+    stringChar.parser = letter | digit | space | symbol | backslash >>> (backslash | quote | stringChar)
+    let star = str("*")
+    let slash = str("/")
+    let percent = str("%")
     let plus = str("+")
     let minus = str("-")
     let lessThanOrEqual = str("<=")
     let greaterThanOrEqual = str(">=")
     let lessThan = str("<")
     let greaterThan = str(">")
+    let equalTo = str("eq")
+    let notEqualTo = str("neq")
+    let logicalAnd = str("and")
+    let logicalOr = str("or")
+//    let notKeyword = str("NOT").tag("not") >>> space
+    let when = str("when") >>> space
+    let const = str("let") >>> space
+    let assign = str("=") >>> skip
 
     let expression = Deferred()
-    let term = Deferred()
-    let pattern = Deferred()
-    let atom = Deferred()
 
-    // Atoms.
+    // Lists.
 
-    // RESERVED_WORDS = %w( yes no not and or if use class eq neq Boolean List String Number )
-    let namePrefix = underscore.maybe >>> lowercaseLetter
-    let nameSuffix = letter | digit | underscore
-    let name = namePrefix >>> nameSuffix.some.maybe
+    let item = expression.tag("item")
+    let tail = expression.tag("tail")
+    let list = lBracket >>> (item >>> (comma >>> item).recur).recur.tag("list") >>> rBracket
+    let heads = (item >>> (comma >>> item).recur).tag("heads")
+    let listConstructor = lBracket >>> heads >>> pipe >>> tail >>> rBracket
 
-    // Literal values.
+    // Literals.
 
-    let stringValue = quote >>> character.recur.tag("stringValue") >>> quote >>> skip
-    let floatValue = (minus.maybe >>> digit.some >>> dot >>> digit.some).tag("floatValue")
-    let integerValue = (minus.maybe >>> digit.some).tag("integerValue")
-    let numericValue = floatValue | integerValue
-    let trueValue = str("yes").tag("trueValue")
-    let falseValue = str("no").tag("falseValue")
+    let trueValue = str("yes").tag("true")
+    let falseValue = str("no").tag("false")
     let booleanValue = trueValue | falseValue
-    let list = (lBracket >>> (expression.tag("listItem") >>> (comma >>> expression.tag("listItem")).recur).maybe.tag("list") >>> rBracket)
-    let listConstruction = (lBracket >>> expression.tag("listItem") >>> pipe >>> expression.tag("list") >>> rBracket)
-    let literalValue = booleanValue | numericValue | stringValue | list | listConstruction
+    let integerValue = (minus.maybe >>> digit.some).tag("integer")
+    let floatValue = (minus.maybe >>> digit.some >>> dot >>> digit.some).tag("float")
+    let numericValue = floatValue | integerValue
+    let stringValue = quote >>> stringChar.recur.tag("string") >>> quote >>> skip
 
-    // Patterns.
-
-    let head = pattern.tag("head")
-    let tail = pattern.tag("tail")
-    let heads = (head >>> (comma >>> head).recur).tag("heads")
-    let listPattern = lBracket >>> heads >>> pipe >>> tail >>> rBracket
-    let listParamPattern = lBracket >>> (pattern.tag("listItem") >>> (comma >>> pattern.tag("listItem")).recur).maybe.tag("list") >>> rBracket
-    pattern.parser = listParamPattern | listPattern | literalValue.tag("literal") | name.tag("variable")
+    let literalValue = booleanValue | numericValue | stringValue | list | listConstructor
 
     // Expressions.
 
-    let multiplicativeExpression = term.tag("left") >>> (skip >>> (times | dividedBy | modulo).tag("op") >>> skip >>> term.tag("right")).recur.tag("ops")
+    let relational = Deferred()
+    let equality = Deferred()
+    let conjunctive = Deferred()
+    let disjunctive = Deferred()
+    let term = Deferred()
 
-    let additiveExpression = multiplicativeExpression.tag("left") >>> (skip >>> (plus | minus).tag("op") >>> skip >>> multiplicativeExpression.tag("right")).recur.tag("ops")
+    let multiplicative = term.tag("left") >>> (skip >>> (star | slash | percent).tag("op") >>> skip >>> term.tag("right")).recur.tag("ops")
+    let additive = multiplicative.tag("left") >>> (skip >>> (plus | minus).tag("op") >>> skip >>> multiplicative.tag("right")).recur.tag("ops")
+    relational.parser = additive.tag("left") >>> (skip >>> (lessThanOrEqual | greaterThanOrEqual | lessThan | greaterThan).tag("op") >>> skip >>> relational.tag("right")).recur.tag("ops")
+    equality.parser = relational.tag("left") >>> (space >>> (equalTo | notEqualTo).tag("op") >>> space >>> equality.tag("right")).recur.tag("ops")
+    conjunctive.parser = equality.tag("left") >>> (space >>> logicalAnd.tag("op") >>> space >>> conjunctive.tag("right")).recur.tag("ops")
+    disjunctive.parser = conjunctive.tag("left") >>> (space >>> logicalOr.tag("op") >>> space >>> disjunctive.tag("right")).recur.tag("ops")
 
-    let relationalExpression = Deferred()
-    relationalExpression.parser = additiveExpression.tag("left") >>> (skip >>> (lessThanOrEqual | greaterThanOrEqual | lessThan | greaterThan).tag("op") >>> skip >>> relationalExpression.tag("right")).recur.tag("ops")
+    expression.parser = disjunctive.parser
 
-    let equals = str("eq")
-    let notEquals = str("neq")
-    let equalityExpression = Deferred()
-    equalityExpression.parser = relationalExpression.tag("left") >>> (space >>> (equals | notEquals).tag("op") >>> space >>> equalityExpression.tag("right")).recur.tag("ops")
+    // Atoms.
 
-    let andKeyword = str("and")
-    let andExpression = Deferred()
-    andExpression.parser = equalityExpression.tag("left") >>> (space >>> andKeyword.tag("op") >>> space >>> andExpression.tag("right")).recur.tag("ops")
+    let namePrefix = underscore.maybe >>> lowercaseLetter
+    let nameSuffix = letter | digit | underscore
+    let name = namePrefix >>> nameSuffix.some.maybe
+    let variableName = name.tag("variableName") >>> skip
+    let functionName = name.tag("functionName")
 
-    let orKeyword = str("or")
-    let orExpression = Deferred()
-    orExpression.parser = andExpression.tag("left") >>> (space >>> orKeyword.tag("op") >>> space >>> orExpression.tag("right")).recur.tag("ops")
+    // Function calls.
 
-    expression.parser = orExpression.parser
+    let atom = Deferred()
+    let arg = expression.tag("arg")
+    let args = (arg >>> (comma >>> arg).recur).tag("args")
+    let freeFunctionCall = functionName >>> (lParen >>> args.maybe >>> skip >>> rParen)
+    let subjectFunctionCall = atom.tag("subject") >>> (dot >>> functionName >>> (lParen >>> args.maybe >>> rParen).maybe).some.tag("calls")
+    let functionCall = freeFunctionCall | subjectFunctionCall
 
-    // Function chains.
+    // Function declarations.
 
-    let functionArguments = expression.tag("arg") >>> (comma >>> expression.tag("arg")).recur
-    let functionCall = dot >>> name.tag("funcName") >>> (lParen >>> functionArguments.tag("args").maybe >>> rParen).maybe
-    let functionChain = atom.tag("subject") >>> functionCall.some.tag("calls")
-
-    let freeFunctionCall = name.tag("funcName") >>> (lParen >>> functionArguments.tag("args").maybe >>> rParen)
-
-    // Functions.
-
-    let parameter = pattern
-    let functionSubject = parameter.tag("param").tag("subject")
-    let functionName = name.tag("funcName")
-    let parameters = parameter.tag("param") >>> (comma >>> parameter.tag("param")).recur
+    let parameter = (listConstructor | list | literalValue | variableName).tag("param")
+    let functionSubject = parameter.tag("subject")
+    let parameters = parameter >>> (comma >>> parameter).recur
     let functionParameters = lParen >>> parameters.recur(0, 1).tag("params") >>> rParen
-    let assign = skip >>> str("=") >>> skip
     let functionBody = expression.tag("body") >>> skip
-    let ifKeyword = space >>> str("when") >>> space
-    let guardClause = (ifKeyword >>> expression).maybe.tag("guard")
-    let subjectFunction = functionSubject >>> dot >>> functionName >>> functionParameters.maybe >>> guardClause >>> assign >>> functionBody
+    let guardClause = (space >>> when >>> expression).maybe.tag("guard") >>> skip
+    let subjectFunctionDecl = functionSubject >>> dot >>> functionName >>> functionParameters.maybe >>> guardClause >>> assign >>> functionBody
 
-    let freeFunction = functionName >>> functionParameters >>> guardClause >>> assign >>> functionBody
+    let freeFunctionDecl = functionName >>> functionParameters >>> guardClause >>> assign >>> functionBody
 
-    let function = subjectFunction | freeFunction
+    let functionDecl = subjectFunctionDecl | freeFunctionDecl
 
     // Lambdas.
 
     let lambdaParameters = pipe >>> parameters.tag("params") >>> pipe
     let lambdaBody = expression.tag("lambdaBody") >>> skip
-    let lambda = (lambdaParameters >>> lambdaBody).tag("lambda")
+    let lambda = lambdaParameters >>> lambdaBody
 
-    // Let.
+    // Constants.
 
-    let letExpr = str("let") >>> skip >>> (name.tag("variable") >>> assign >>> expression.tag("body")).tag("let")
+    let constant = const >>> variableName >>> assign >>> expression.tag("constBody")
 
     // Terms.
 
-    let wrappedExpression = lParen >>> expression.tag("expression") >>> rParen
-    atom.parser = wrappedExpression | freeFunctionCall | literalValue | name.tag("variable")// | listPattern | name
+    let wrappedExpression = lParen >>> expression.tag("wrapped") >>> rParen
+    atom.parser = wrappedExpression | freeFunctionCall | literalValue | variableName
 
-    //    let notKeyword = str("NOT").tag("not") >>> space
     //    let negatedTerm = notKeyword >>> term.tag("negatedTerm")
-    //    term.parser = negatedTerm | functionChain | lambda | atom
-    term.parser = functionChain | freeFunctionCall | lambda | letExpr | atom.parser!
-
-    // Imports.
-
-//    let importKeyword = str("Use") >>> space
-//    let importFilename = stringValue
-//    let `import` = importKeyword >>> importFilename.tag("import")
-
-    // Classes.
-
-//    let numberKeyword = str("Number") >>> skip
-//    let booleanKeyword = str("Boolean") >>> skip
-//    let stringKeyword = str("String") >>> skip
-//    let listKeyword = str("List") >>> skip
-//    let className = numberKeyword | booleanKeyword | stringKeyword | listKeyword
-//    let classKeyword = str("Class") >>> space
-//    let classDeclaration = classKeyword >>> className.tag("subjectType")
+    term.parser = /*negatedTerm |*/ functionCall | lambda | constant | atom
 
     // Root.
 
-//    let statement = classDeclaration >>> `import` >>> function >>> expression
-//    let program = skip >>> statement
+    let root = functionDecl | expression
 
-    return function | expression
+    return root
 }
--- Sources/Song/Transformer.swift
+++ Sources/Song/Transformer.swift
@@ -3,153 +3,61 @@ import Syft
 public enum SongTransformError: Error {
     case unknown
     case notNumeric(String)
-    case notAList(Expression)
-    case noFunctionCalls
+    case notAFunction
+    case notAFunctionCall
 }
 
 public func makeTransformer() -> Transformer<Expression> {
     let t = Transformer<Expression>()
 
-    t.rule(["trueValue": .simple("")]) { _ in
+    // Lists.
+
+    t.rule(["item": .simple("item")]) {
+        try $0.val("item")
+    }
+
+    t.rule(["tail": .simple("tail")]) {
+        try $0.val("tail")
+    }
+
+    t.rule(["list": .series("items")]) {
+        .list(try $0.vals("items"))
+    }
+
+    t.rule(["heads": .series("heads"), "tail": .simple("tail")]) {
+        .listConstructor(try $0.vals("heads"), try $0.val("tail"))
+    }
+
+    // Literals.
+
+    t.rule(["true": .simple("")]) { _ in
         .booleanValue(true)
     }
 
-    t.rule(["falseValue": .simple("")]) { _ in
+    t.rule(["false": .simple("")]) { _ in
         .booleanValue(false)
     }
 
-    t.rule(["stringValue": .simple("s")]) {
-        .stringValue(try $0.str("s"))
-    }
-
-    t.rule(["integerValue": .simple("i")]) {
+    t.rule(["integer": .simple("i")]) {
         let i = try $0.str("i")
         guard let int = Int(i) else { throw SongTransformError.notNumeric(i) }
         return .integerValue(int)
     }
 
-    t.rule(["floatValue": .simple("f")]) {
+    t.rule(["float": .simple("f")]) {
         let f = try $0.str("f")
         guard let float = Double(f) else { throw SongTransformError.notNumeric(f) }
         return .floatValue(float)
     }
 
+    t.rule(["string": .simple("s")]) {
+        .stringValue(try $0.str("s"))
+    }
+
+    // Expressions.
+
     t.rule(["right": .simple("right"), "op": .simple("op")]) {
-        let right = try $0.val("right")
-        let op = try $0.str("op")
-        return Expression.call(name: op, arguments: [right])
-    }
-
-    t.rule(["expression": .simple("e")]) {
-        return try $0.val("e")
-    }
-
-    t.rule(["listItem": .simple("item")]) {
-        return try $0.val("item")
-    }
-
-    t.rule(["variable": .simple("v")]) {
-        let v = try $0.str("v")
-        return Expression.variable(v)
-    }
-
-    t.rule(["arg": .simple("arg")]) {
-        return try $0.val("arg")
-    }
-
-    t.rule(["funcName": .simple("funcName"), "args": .series("args")]) {
-        let funcName = try $0.str("funcName")
-        let args = try $0.vals("args")
-        return Expression.call(name: funcName, arguments: args)
-    }
-
-    t.rule(["funcName": .simple("funcName")]) {
-        let funcName = try $0.str("funcName")
-        return Expression.call(name: funcName, arguments: [])
-    }
-
-    t.rule(["subject": .simple("subject"), "calls": .series("calls")]) {
-        let subject = try $0.val("subject")
-        var calls = try $0.vals("calls")
-        guard calls.count > 0 else { throw SongTransformError.noFunctionCalls }
-        let firstCall = calls.removeFirst()
-        guard case let .call(call) = firstCall else { throw SongTransformError.noFunctionCalls }
-        let arguments = [subject] + call.arguments
-        let firstFuncCall = Expression.call(name: call.name, arguments: arguments)
-        return calls.reduce(firstFuncCall) { acc, next in
-            guard case .call(let name, var arguments) = next else {
-                preconditionFailure("not a function call")
-            }
-            arguments.insert(acc, at: 0)
-            return Expression.call(name: name, arguments: arguments)
-        }
-    }
-
-    t.rule(["literal": .simple("literal")]) {
-        try $0.val("literal")
-    }
-
-    t.rule(["param": .simple("param")]) {
-        try $0.val("param")
-    }
-
-    t.rule(["funcName": .simple("funcName"), "body": .simple("body"), "guard": .simple("guard"), "subject": .simple("subject")]) {
-        Expression.subfunction(try transformFunction(args: $0))
-    }
-
-    t.rule(["funcName": .simple("funcName"), "body": .simple("body"), "guard": .simple("guard"), "params": .series("params")]) {
-        Expression.subfunction(try transformFunction(args: $0))
-    }
-
-    t.rule(["funcName": .simple("funcName"), "body": .simple("body"), "guard": .simple("guard"), "subject": .simple("subject"), "params": .series("params")]) {
-        Expression.subfunction(try transformFunction(args: $0))
-    }
-
-    t.rule(["lambdaBody": .simple("body"), "params": .series("params")]) {
-        let subfunction = try transformFunction(args: $0)
-        let closure = Expression.subfunction(subfunction)
-        return closure
-    }
-
-    t.rule(["lambda": .simple("lambda")]) {
-        try $0.val("lambda")
-    }
-
-    func transformFunction(args: TransformerReducerArguments<Expression>) throws -> Subfunction {
-        var funcName: String?
-        do {
-            funcName = try args.str("funcName")
-        } catch {}
-        var params = [Expression]()
-        do {
-            params = try args.vals("params")
-        } catch {}
-        do {
-            let subject = try args.val("subject")
-            params.insert(subject, at: 0)
-        } catch {}
-        var when = Expression.booleanValue(true)
-        do {
-            when = try args.val("guard")
-        } catch {}
-        let body = try args.val("body")
-        let subfunction = Subfunction(name: funcName, patterns: params, when: when, body: body)
-        return subfunction
-    }
-
-    t.rule(["list": .series("items")]) {
-        let items = try $0.vals("items")
-        return Expression.list(items)
-    }
-
-    t.rule(["list": .simple("")]) { _ in
-        Expression.list([])
-    }
-
-    t.rule(["listItem": .simple("head"), "list": .simple("tail")]) {
-        let head = try $0.val("head")
-        let tail = try $0.val("tail")
-        return Expression.listConstructor([head], tail)
+        .call(name: try $0.str("op"), arguments: [try $0.val("right")])
     }
 
     t.rule(["left": .simple("left"), "ops": .series("ops")]) {
@@ -158,38 +66,112 @@ public func makeTransformer() -> Transformer<Expression> {
         guard ops.count > 0 else { return left }
         let first = ops.removeFirst()
         guard case .call(let name, var arguments) = first else {
-            preconditionFailure("not a function call")
+            throw SongTransformError.notAFunctionCall
         }
         arguments.insert(left, at: 0)
         let firstFuncCall = Expression.call(name: name, arguments: arguments)
-        return ops.reduce(firstFuncCall) { acc, next in
+        return try ops.reduce(firstFuncCall) { acc, next in
             guard case .call(let name, var arguments) = next else {
-                preconditionFailure("not a function call")
+                throw SongTransformError.notAFunctionCall
             }
             arguments.insert(acc, at: 0)
             return Expression.call(name: name, arguments: arguments)
         }
     }
 
-    t.rule(["body": .simple("body"), "variable": .simple("name")]) {
-        let name = try $0.str("name")
-        let body = try $0.val("body")
-        return Expression.constant(name: name, value: body)
+    // Atoms.
+
+    t.rule(["variableName": .simple("v")]) {
+        .variable(try $0.str("v"))
     }
 
-    t.rule(["let": .simple("let")]) {
-        try $0.val("let")
+    t.rule(["functionName": .simple("name")]) {
+        .call(name: try $0.str("name"), arguments: [])
     }
 
-    t.rule(["head": .simple("head")]) {
-        try $0.val("head")
+    // Function calls.
+
+    t.rule(["arg": .simple("arg")]) {
+        try $0.val("arg")
     }
 
-    t.rule(["heads": .series("heads"), "tail": .simple("tail")]) {
-        let heads = try $0.vals("heads")
-        let tail = try $0.val("tail")
-        return Expression.listConstructor(heads, tail)
+    t.rule(["functionName": .simple("name"), "args": .series("args")]) {
+        .call(name: try $0.str("name"), arguments: try $0.vals("args"))
+    }
+
+    t.rule(["subject": .simple("subject"), "calls": .series("calls")]) {
+        let subject = try $0.val("subject")
+        var calls = try $0.vals("calls")
+        guard calls.count > 0 else { throw SongTransformError.notAFunction }
+        let firstCall = calls.removeFirst()
+        guard case let .call(call) = firstCall else { throw SongTransformError.notAFunction }
+        let arguments = [subject] + call.arguments
+        let firstFuncCall = Expression.call(name: call.name, arguments: arguments)
+        return try calls.reduce(firstFuncCall) { acc, next in
+            guard case .call(let name, var arguments) = next else {
+                throw SongTransformError.notAFunction
+            }
+            arguments.insert(acc, at: 0)
+            return Expression.call(name: name, arguments: arguments)
+        }
+    }
+
+    // Function declarations.
+
+    t.rule(["param": .simple("param")]) {
+        try $0.val("param")
+    }
+
+    t.rule(["functionName": .simple("name"), "body": .simple("body"), "guard": .simple("guard"), "subject": .simple("subject")]) {
+        .subfunction(try transformFunction(args: $0))
+    }
+
+    t.rule(["functionName": .simple("name"), "body": .simple("body"), "guard": .simple("guard"), "params": .series("params")]) {
+        .subfunction(try transformFunction(args: $0))
+    }
+
+    t.rule(["subject": .simple("subject"), "functionName": .simple("name"), "body": .simple("body"), "guard": .simple("guard"), "params": .series("params")]) {
+        .subfunction(try transformFunction(args: $0))
+    }
+
+    // Lambdas.
+
+    t.rule(["lambdaBody": .simple("body"), "params": .series("params")]) {
+        .subfunction(try transformFunction(args: $0))
+    }
+
+    // Constants.
+
+    t.rule(["constBody": .simple("body"), "variableName": .simple("name")]) {
+        .constant(name: try $0.str("name"), value: try $0.val("body"))
+    }
+
+    // Terms.
+
+    t.rule(["wrapped": .simple("e")]) {
+        try $0.val("e")
     }
 
     return t
+}
+
+func transformFunction(args: TransformerReducerArguments<Expression>) throws -> Subfunction {
+    var name: String?
+    do {
+        name = try args.str("name")
+    } catch {}
+    var params = [Expression]()
+    do {
+        params = try args.vals("params")
+    } catch {}
+    do {
+        let subject = try args.val("subject")
+        params.insert(subject, at: 0)
+    } catch {}
+    var when = Expression.booleanValue(true)
+    do {
+        when = try args.val("guard")
+    } catch {}
+    let body = try args.val("body")
+    return Subfunction(name: name, patterns: params, when: when, body: body)
 }
