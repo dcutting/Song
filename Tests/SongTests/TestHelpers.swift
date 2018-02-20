4f1f70ad6f7988142269a135950cd08dadb1c388
Dan Cutting <dan@cutting.io>
2018-02-20 16:55:15 +0000

    Helpers for easily testing lots of parse strings.

--- Tests/SongTests/TestHelpers.swift
+++ Tests/SongTests/TestHelpers.swift
@@ -1,9 +1,27 @@
 import XCTest
+import Song
 
 func assertNoThrow(file: StaticString = #file, line: UInt = #line, _ closure: () throws -> Void) {
     do {
         try closure()
     } catch {
         XCTFail("\(error)", file: file, line: line)
+    }
+}
+
+extension String {
+
+    func becomes(_ expected: Expression, file: StaticString = #file, line: UInt = #line) {
+        assertNoThrow {
+            let actual = try parse(self)
+            XCTAssertEqual(expected, actual, file: file, line: line)
+        }
+    }
+
+    private func parse(_ line: String) throws -> Expression {
+        let parser = makeParser()
+        let transformer = makeTransformer()
+        let result = parser.parse(line)
+        return try transformer.transform(result)
     }
 }
