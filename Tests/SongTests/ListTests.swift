68140603d1bee15e3c777a584c402310e53e2599
Dan Cutting <dan@cutting.io>
2018-02-21 13:36:05 +0000

    Test list evaluation.

--- Tests/SongTests/ListTests.swift
+++ Tests/SongTests/ListTests.swift
@@ -24,4 +24,23 @@ class ListTests: XCTestCase {
         let right = Expression.list([.integerValue(1), .integerValue(2), .integerValue(3)])
         XCTAssertNotEqual(left, right)
     }
+
+    func test_evaluate_emptyList_returnsEmptyList() {
+        XCTAssertEqual(Expression.list([]), Expression.list([]))
+    }
+
+    func test_evaluate_nonEmptyList_returnsListOfAllEvaluatedItems() {
+        let list = Expression.list([
+            .call(name: "+", arguments: [.integerValue(2), .integerValue(3)]),
+            .variable("x")
+            ])
+        assertNoThrow {
+            let actual = try list.evaluate(context: ["x": [.booleanValue(false)]])
+            let expected = Expression.list([
+                .integerValue(5),
+                .booleanValue(false)
+                ])
+            XCTAssertEqual(expected, actual)
+        }
+    }
 }
