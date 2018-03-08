ed88128d96429b8ef21d9a851e0e10ca3265a9fd
Dan Cutting <dan@cutting.io>
2018-03-08 16:55:47 +0000

    Uncovered some parser bugs.

--- Tests/SongTests/Parser/LambdaParserTests.swift
+++ Tests/SongTests/Parser/LambdaParserTests.swift
@@ -4,7 +4,9 @@ import Song
 class LambdaParserTests: XCTestCase {
 
     func test_shouldParse() {
-        "|x| x".makes(.function(Function(name: nil, patterns: [.name("x")], when: .bool(true), body: .name("x"))))
+        "|x| x".makes(.lambda([.name("x")], .name("x")))
+        "|x| (x)".makes(.lambda([.name("x")], .name("x")))
+        "|x| x < 5".makes(.lambda([.name("x")], .call("<", [.name("x"), .int(5)])))
         "| x , y | x".makes(.function(Function(name: nil, patterns: [.name("x"), .name("y")], when: .bool(true), body: .name("x"))))
 """
  |
