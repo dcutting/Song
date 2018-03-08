356c869d4b136e5db777b2fdc0199b96ede403cd
Dan Cutting <dan@cutting.io>
2018-03-08 20:02:44 +0000

    Restrict lambdas from matching literals apart from lists and list constructors.

--- Sources/Song/Parser/Parser.swift
+++ Sources/Song/Parser/Parser.swift
@@ -94,6 +94,13 @@ public func makeParser() -> ParserProtocol {
     let parameterDelimiter = skipSpaceAndNewlines >>> comma >>> skipSpaceAndNewlines
     let parameters = parameter >>> (parameterDelimiter >>> parameter).recur
 
+    // Lambdas.
+
+    let lambdaParameter = (list | listConstructor | variableName).tag("param")
+    let lambdaParameters = lambdaParameter >>> (parameterDelimiter >>> lambdaParameter).recur
+
+    let lambda = (pipe >>> skipSpaceAndNewlines >>> lambdaParameters.recur(0, 1).tag("params") >>> skipSpaceAndNewlines >>> pipe >>> skipSpaceAndNewlines >>> expression.tag("body")).tag("lambda")
+
     // Function calls.
 
     let call = Deferred()
@@ -102,8 +109,6 @@ public func makeParser() -> ParserProtocol {
     let argumentDelimiter = skipSpaceAndNewlines >>> comma >>> skipSpaceAndNewlines
     let arguments = argument >>> (argumentDelimiter >>> argument).recur
     let wrappedArguments = lParen >>> skipSpaceAndNewlines >>> arguments.recur.tag("args") >>> skipSpaceAndNewlines >>> rParen
-
-    let lambda = (pipe >>> skipSpaceAndNewlines >>> parameters.recur(0, 1).tag("params") >>> skipSpaceAndNewlines >>> pipe >>> skipSpaceAndNewlines >>> expression.tag("body")).tag("lambda")
 
     let callable = call | variableName | lambda | scope
 
--- Tests/SongTests/Parser/LambdaParserTests.swift
+++ Tests/SongTests/Parser/LambdaParserTests.swift
@@ -19,5 +19,11 @@ class LambdaParserTests: XCTestCase {
         "|[x|xs], y| x".makes(.function(Function(name: nil, patterns: [.cons([.name("x")], .name("xs")), .name("y")], when: .bool(true), body: .name("x"))))
         "|_| 5".makes(.function(Function(name: nil, patterns: [.ignore], when: .bool(true), body: .int(5))))
         "|| 5".makes(.function(Function(name: nil, patterns: [], when: .bool(true), body: .int(5))))
+        "|[x, y], z| x".makes(.lambda([.list([.name("x"), .name("y")]), .name("z")], .name("x")))
+    }
+
+    func test_shouldNotParse() {
+        "|(x)| x".fails()
+        "|1| x".fails()
     }
 }
