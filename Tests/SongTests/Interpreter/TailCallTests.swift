61a151909af125f44cca6c9963dcea4a16d0c86f
Dan Cutting <dan@cutting.io>
2018-03-31 21:25:03 +0100

    Tail call description.

--- Tests/SongTests/Interpreter/TailCallTests.swift
+++ Tests/SongTests/Interpreter/TailCallTests.swift
@@ -3,6 +3,13 @@ import Song
 
 class TailCallTests: XCTestCase {
 
+    func test_description_tailCall() {
+        let context = extendContext(context: Context(), name: "x", value: .int(5))
+        let tailCall = Expression.tailCall("recur", [.int(0)], context)
+        let result = "\(tailCall)"
+        XCTAssertEqual("[(x: 5) recur(0)]", result)
+    }
+
     func test_deepRecursion_bodyIsTailCall() {
         assertNoThrow {
             let fb = Function(name: "times", patterns: [.name("n")], when: .call("Eq", [.name("n"), .int(0)]), body: .name("n"))
