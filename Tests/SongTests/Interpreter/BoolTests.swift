b5fbd5f278e167b79a700cd8f65a8c966460cb6a
Dan Cutting <dan@cutting.io>
2018-04-03 00:27:00 +0100

    Use yes/no helpers.

--- Tests/SongTests/Interpreter/BoolTests.swift
+++ Tests/SongTests/Interpreter/BoolTests.swift
@@ -2,54 +2,51 @@ import XCTest
 import Song
 
 class BoolTests: XCTestCase {
-    
-    let trueBool = Expression.yes
-    let falseBool = Expression.no
-    
+
     func test_description_true() {
-        XCTAssertEqual("Yes", "\(trueBool)")
+        XCTAssertEqual("Yes", "\(Expression.yes)")
     }
     
     func test_description_false() {
-        XCTAssertEqual("No", "\(falseBool)")
+        XCTAssertEqual("No", "\(Expression.no)")
     }
     
     func test_evaluate_true() {
         assertNoThrow {
-            XCTAssertEqual(trueBool, try trueBool.evaluate())
+            XCTAssertEqual(Expression.yes, try Expression.yes.evaluate())
         }
     }
     
     func test_evaluate_false() {
         assertNoThrow {
-            XCTAssertEqual(falseBool, try falseBool.evaluate())
+            XCTAssertEqual(Expression.no, try Expression.no.evaluate())
         }
     }
 
     func test_eq_equal_returnsYes() {
         assertNoThrow {
-            let call = Expression.call("Eq", [trueBool, trueBool])
+            let call = Expression.call("Eq", [.yes, .yes])
             XCTAssertEqual(Expression.yes, try call.evaluate())
         }
     }
 
     func test_eq_unequal_returnsNo() {
         assertNoThrow {
-            let call = Expression.call("Eq", [falseBool, trueBool])
+            let call = Expression.call("Eq", [.no, .yes])
             XCTAssertEqual(Expression.no, try call.evaluate())
         }
     }
 
     func test_neq_equal_returnsNo() {
         assertNoThrow {
-            let call = Expression.call("Neq", [trueBool, trueBool])
+            let call = Expression.call("Neq", [.yes, .yes])
             XCTAssertEqual(Expression.no, try call.evaluate())
         }
     }
 
     func test_neq_unequal_returnsYes() {
         assertNoThrow {
-            let call = Expression.call("Neq", [falseBool, trueBool])
+            let call = Expression.call("Neq", [.no, .yes])
             XCTAssertEqual(Expression.yes, try call.evaluate())
         }
     }
